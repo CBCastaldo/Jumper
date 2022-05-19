6498f07b65a39d85b816e1b03758a8ce2621188b
CBCastaldo <90955886+CBCastaldo@users.noreply.github.com>
2022-05-19 15:52:01 -0600

    Moving Devin's Base Files to their own repository.

---
+++ game/parachute.py
@@ -0,0 +1,16 @@
+import random
+
+class Parachute:
+
+    def __init__(self):
+        self._word_list = []
+        self._word = self._word_list(random.randint(0, (len(self._word_list) - 1)))
+        self._lives = 4
+        self._parachute = ""
+        #We would have to put the parachute image above
+
+    def cut_line(self):
+        #Removes a line from parachute if guess doesn't give a new letter
+        return 
+    
+    
