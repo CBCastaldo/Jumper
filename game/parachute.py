b503a7676c744a56187d72e109c366eec5386e6d
CBCastaldo <lady_lacrosse@hotmail.com>
2022-05-25 22:16:14 -0600

    Update parachute.py
    Created Parachute

--- game/parachute.py
+++ game/parachute.py
@@ -8,9 +8,47 @@ class Parachute:
         self._lives = 4
         self._parachute = ""
         #We would have to put the parachute image above
+    
+    def parachute():
+        parachute = ['''  
+  ___
+ /___\\
+ \   /
+  \ /
+   O
+  /|\\
+  / \\
+     
+^^^^^^^''', '''
+ /___\\
+ \   /
+  \ /
+   O
+  /|\\
+  / \\
+    
+^^^^^^^''', '''
+ \   /
+  \ /
+   O
+  /|\\
+  / \\
+     
+^^^^^^^''', '''
+  \ /
+   O
+  /|\\
+  / \\
+     
+^^^^^^^''', '''
+   O
+  /|\\
+  / \\
+     
+^^^^^^^''']
+        for x in range(len(parachute)):
+            print (parachute[x])
 
     def cut_line(self):
         #Removes a line from parachute if guess doesn't give a new letter
         return 
-    
-    
